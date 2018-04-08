4e3cd6d9560835d76ba2ba2a646dab8e5754b120
walk_miao <372815340@qq.com>
2018-04-08 09:08:39 +0800

    requests模块

---
+++ requests_module.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time    : 2018/4/8 9:03
+# @Author  : lch
+# @File    : requests_module.py
+import requests
+#传递url参数
+payload={'key1':'value1','key2':'value2'}
+html=requests.get(url='http://httpbin.org',params=payload)
+print(html.url)
