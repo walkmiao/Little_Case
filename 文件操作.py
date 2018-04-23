d7dff2982d5dabe8b68cd790249b6496e056a0df
walkmiao <372815340@qq.com>
2018-04-23 19:16:50 +0800

    代码改进 类变量改为实例变量并在init函数中初始化

--- 文件操作.py
+++ 文件操作.py
@@ -23,37 +23,37 @@ import os
 
 #编写一个程序，能在指定目录下查找文件名包含指定字符串的文件，并打印出相对路径。
 class findStr():
-    file_list = []
-    str_list = []
     def __init__(self,path):
         self.path=path
+        self.file_list=[]
+        self.str_list=[]
     def get_filepath(self,path):
         if os.path.isdir(path):
             for i in os.listdir(path):
                 if os.path.isdir(os.path.join(path,i))==False:
-                    findStr.file_list.append(os.path.join(path,i))
+                    self.file_list.append(os.path.join(path,i))
                 else:
                     self.get_filepath(os.path.join(path,i))
         else:
-            findStr.file_list.append(path)
-        return findStr.file_list
+            self.file_list.append(path)
+        return self.file_list
 
 
     def find_str(self,anystr,file_list):
         for i in file_list:
             if anystr in i:
-                findStr.str_list.append(i)
-        return findStr.str_list
+                self.str_list.append(i)
+        return self.str_list
     def main(self,anystr):
-        findStr.file_list=self.get_filepath(self.path)
-        findStr.str_list=self.find_str(anystr,findStr.file_list)
-        for i in findStr.str_list:
+        self.file_list=self.get_filepath(self.path)
+        self.str_list=self.find_str(anystr,self.file_list)
+        for i in self.str_list:
             print(i)
 f=findStr('.')
 if __name__=='__main__':
     # print(f.get_filepath(f.path))
     # print(f.find_str('config',f.get_filepath(f.path)))
-    f.main('1.txt')
+    f.main('.py')
 # print(get_filepath('.'))
 # print([os.path.abspath(i) for i in os.listdir('D:\PycharmProjects\Little_Case\.idea')])
 
