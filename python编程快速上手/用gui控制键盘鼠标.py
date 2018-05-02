be725d897bfd42ccc105d0033bfb8ca94f1fa835
walkmiao <372815340@qq.com>
2018-05-02 18:06:00 +0800

    python编程快速上手习题

---
+++ python编程快速上手/用gui控制键盘鼠标.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/5/2 17:56
+# @Author  : LCH
+# @Site    : 
+# @File    : 用gui控制键盘鼠标.py
+# @Software: PyCharm
+import pyautogui
+print(pyautogui.position())
